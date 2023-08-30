f7dad1fe9077d3d2dc4979d83958cf4d92bb7ec8
lawrence-254 <lodhiambo26@gmail.com>
2023-08-30 02:03:17 -0700

    2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,24 +9,24 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *right_child = (binary_tree_t *)malloc(
-			sizeof(binary_tree_t));
+	binary_tree_t *right_child;
 
 	if (!parent)
 		return (NULL);
+	right_child = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (!right_child)
 		return (NULL);
-	right_child->parent = parent;
 	right_child->n = value;
 	right_child->left = NULL;
+	right_child->right = NULL;
 
 	if (parent->right != NULL)
 	{
 		right_child->right = parent->right;
-		parent->right = right_child;
-	}else{
-		right_child->right = NULL;
+		parent->right->parent = right_child;
 	}
 	parent->right = right_child;
+	right_child->parent = parent;
+
 	return (right_child);
 }
